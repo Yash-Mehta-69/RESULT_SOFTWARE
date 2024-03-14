6218ebc617898662763720644534764c0d3939ee
Yash-Mehta-69 <yashm8866@gmail.com>
2024-03-15 00:12:14 +0530

    teacher only login

--- software.py
+++ software.py
@@ -157,179 +157,86 @@ def admin_main():
 def teacher_main(user):
     print(user)
 
-    this_class = user[2]
-    this_year = user[3]
-
-    cur.execute("show tables")
-    tables = cur.fetchall()
-    # print(tables)
-    table_flag = False
-    for i in tables:
-        if i[0] == "{}_{}".format(this_class,this_year):
-            table_flag = True
-        
-    if table_flag==False:
-        print("Table Not Exists")
-        messagebox.showerror("Error","Sorry, We dont have the students data!")
-    else:
-        print("Table Exists")
-        # print(this_year,this_exam,this_class)
-        cur.execute("select * from {}_{}".format(this_class,this_year))
-        students_data = cur.fetchall()
-        total_strength = len(students_data)
-        print(total_strength)
-        print(students_data)
-
-        for widget in signup.winfo_children():
-            widget.destroy() 
-
-        signup.configure(width=700)
-        signup.place_configure(x=400)
-        identify = Label(signup, text = "ENTERING MARKS FOR", padx=5, pady=5, bg="#557A95", font=("Arial Rounded MT Bold",20))
-        identify.place(x=175,y=50)
-
-        identify_1 = Label(signup, text = "CLASS : {}".format(user[2]), padx=5, pady=5, bg="#557A95", font=("Arial Rounded MT Bold",20))
-        identify_1.place(x=250,y=125)
-
-        clicked_exam = StringVar() 
-        exam_options = ["PT 1", "PT 2", "HALF YEARLY", "FINAL EXAMS"]
-        clicked_exam.set(exam_options[0]) # default value
-        exam_MENU = OptionMenu(signup, clicked_exam, *exam_options)
-        exam_MENU.place(x=280,y=300)
-
-        this_exam = clicked_exam.get()
-
-        def ENTER_MARKS():
-            for widget in root.winfo_children():
-                widget.destroy()
-
-
-        ENTER_BTN = Button(signup,width=20,pady=7,text="Enter ->",command=ENTER_MARKS,bg='#57a1f8',fg="white",cursor="hand2",border=0)
-        ENTER_BTN.place(x=250,y=375)
-    
-
-    
-
-def admin_login():
-
-    for widget in signup.winfo_children():
-        widget.destroy() 
-    def sign_in():
-        id=id_entry.get()
-        password=code.get()
-
-        if id=="admin" and password== "Admin@321":
-            messagebox.showinfo("SUCCESS","Login Successful")
-            admin_main()
-        if id!="admin" or password!= "Admin@321":
-            messagebox.showerror("ERROR","Invalid Username Or Password")
-            
-
-    def on_enter(e):
-        id_entry.delete(0,'end')
-    def on_leave(e):
-        name=id_entry.get()
-        if name=='':
-            id_entry.insert(0,"id")
-
-    id_entry = Entry(signup,width=35,border=0,bg="#557A95", fg="white", font=("Microsoft yaHei UI light",11),justify="center")
-    id_entry.place(x=100,y=175)
-    id_entry.insert(0,"ID")
-    id_entry.bind("<FocusIn>",on_enter)
-    id_entry.bind("<FocusOut>",on_leave)
-    Frame(signup,width=272,height=2,bg="white").place(x=105,y=197)
-
-    def on_enter(e):
-            code.delete(0,'end')
-    def on_leave(e):
-        name=code.get()
-        if name=='':
-            code.insert(0,"Password")
-
-    code=Entry(signup,width=35,border=0,bg="#557A95", fg="white", font=("Microsoft yaHei UI light",11),justify="center")
-    code.place(x=100,y=225)
-    code.insert(0,"Password")
-    code.bind("<FocusIn>",on_enter)
-    code.bind("<FocusOut>",on_leave)
-    Frame(signup,width=272,height=2,bg="white").place(x=105,y=247)
-
-
-    Button(signup,width=30,pady=7,text="Sign in",command=sign_in,bg='#57a1f8',fg="white",cursor="hand2",border=0).place(x=130,y=279)
-    back=Button(signup,command=start,pady=1,text="Back",bg='#557A95', fg="white",activebackground='#557A95',activeforeground="white",relief=FLAT,font=("Microsoft yaHei UI light",11))
-    back.place(x=25,y=450)
 
 
 
 
 def teacher_login():
-    
-    for widget in signup.winfo_children():
-        widget.destroy()
+    signup = Frame(root, width=1000, height=500, bg="#B6D0E2")
+    signup.place(x=(screenwidth // 2) - 500, y=(screenheight // 2) - 250)
 
     def sign_in():
-        id=id_entry.get()
-        password=code.get()
+        id1 = id_entry.get()
+        password = code.get()
         year = clicked_year.get()
 
-        cur.execute("select * from teachers where id=%s and password=%s and year=%s",(id,password,year))
+        cur.execute("select * from teachers where TID=%s and TPASS=%s and YEAR=%s", (id1, password, year))
         user = cur.fetchone()
+
         if user:
             messagebox.showinfo("Success", "Login successful!")
-            teacher_main(user)
-        else : 
-            messagebox.showerror("ERROR","Invalid Id Or Password")
+            if user[0] == '1' and user[2] == "Admin@321":
+                admin_main()
+            else:
+                teacher_main(user)
+        else:
+            messagebox.showerror("ERROR", "Invalid Id Or Password")
 
     def on_enter(e):
-        id_entry.delete(0,'end')
+        id_entry.delete(0, 'end')
+
     def on_leave(e):
-        name=id_entry.get()
-        if name=='':
-            id_entry.insert(0,"id")
+        name = id_entry.get()
+        if name == '':
+            id_entry.insert(0, "ID")
 
-    id_entry=Entry(signup,width=35,border=0,bg="#557A95", fg="white",font=("Microsoft yaHei UI light",11),justify="center")
-    id_entry.place(x=100,y=175)
-    id_entry.insert(0,"ID")
-    id_entry.bind("<FocusIn>",on_enter)
-    id_entry.bind("<FocusOut>",on_leave)
-    Frame(signup,width=272,height=2,bg="white").place(x=105,y=197)
+    def on_enter_password(e):
+        code.delete(0, 'end')
 
-    def on_enter(e):
-            code.delete(0,'end')
-    def on_leave(e):
-        name=code.get()
-        if name=='':
-            code.insert(0,"Password")
+    def on_leave_password(e):
+        name = code.get()
+        if name == '':
+            code.insert(0, "Password")
 
-    code=Entry(signup,width=35,border=0,bg="#557A95", fg="white",font=("Microsoft yaHei UI light",11),justify="center")
-    code.place(x=100,y=225)
-    code.insert(0,"Password")
-    code.bind("<FocusIn>",on_enter)
-    code.bind("<FocusOut>",on_leave)
-    Frame(signup,width=272,height=2,bg="white").place(x=105,y=247)
+    bgmainteacher = PhotoImage(file="ICONS/banner.png")
+    background_label = Label(signup, image=bgmainteacher)
+    background_label.place(x=0, y=0)
 
-    clicked_year = StringVar() 
-    year_options = ["2021_22","2022_23","2023_24"]
-    clicked_year.set(year_options[-1]) # default value
+    identify = Label(signup, text="Account Login", padx=5, pady=5, bg="white", fg="blue",
+                     font=("Arial Rounded MT Bold", 20))
+    identify.place(x=640, y=30)
+
+    id_entry = Entry(signup, width=35, border=1, bg="#F0F0F0", fg="black", font=("Microsoft yaHei UI light", 11),
+                     justify="center")
+    id_entry.place(x=600, y=140)
+    id_entry.insert(0, "ID")
+    id_entry.bind("<FocusIn>", on_enter)
+    id_entry.bind("<FocusOut>", on_leave)
+
+    code = Entry(signup, width=35, border=1, bg="#F0F0F0", fg="black", font=("Microsoft yaHei UI light", 11),
+                 justify="center")
+    code.place(x=600, y=200)
+    code.insert(0, "Password")
+    code.bind("<FocusIn>", on_enter_password)
+    code.bind("<FocusOut>", on_leave_password)
+
+    clicked_year = StringVar()
+    year_options = ["2021_22", "2022_23", "2023_24"]
+    clicked_year.set(year_options[-1])  # default value
+
     YEAR_MENU = OptionMenu(signup, clicked_year, *year_options)
-    YEAR_MENU.place(x=200,y=275)
+    YEAR_MENU.place(x=700, y=260)
+    YEAR_MENU.configure(background='#F0F0F0', border=1)
 
-    Button(signup,width=30,pady=7,text="Sign in",command=sign_in,bg='#57a1f8',fg="white",cursor="hand2",border=0).place(x=130,y=329)
-    back=Button(signup,command=start,pady=1,text="Back",bg='#557A95', fg="white",activebackground='#557A95', activeforeground="white",relief=FLAT,font=("Microsoft yaHei UI light",11))
-    back.place(x=25,y=450)
+    Button(signup, width=30, pady=7, text="Sign in", command=sign_in, bg='#F0F0F0', fg="black", cursor="hand2",
+           border=0).place(x=630, y=320)
+
+    root.mainloop()
+
+
+teacher_login()
 
 
 
-
-def start():
-    for widget in signup.winfo_children():
-        widget.destroy()
-    identify = Label(signup, text = "LOGIN AS", padx=5, pady=5, bg="#557A95", fg="white", font=("Arial Rounded MT Bold",20))
-    identify.place(x=180,y=50)
-
-    ADMIN_BTN=Button(signup,bg="#557A95", fg="white",command=admin_login,compound=TOP,text="ADMIN",padx=5,pady=5,activebackground='#557A95', activeforeground="white", relief=FLAT, font=("Arial Rounded MT Bold",20))
-    ADMIN_BTN.place(x=200,y=200)
-    TEACHER_BTN=Button(signup,bg="#557A95", fg="white",compound=TOP,command=teacher_login,text="TEACHER",padx=5,pady=5,activebackground='#557A95', activeforeground="white", relief=FLAT, font=("Arial Rounded MT Bold",20))
-    TEACHER_BTN.place(x=175,y=300)
-start()
 
 root.mainloop()
